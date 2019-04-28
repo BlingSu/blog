a1d63c61c933378913dd46e9abd5f88a83a20647
angelasubi <21978301@qq.com>
2019-04-28 16:32:20 +0800

    feat: length of last word

---
+++ leetcode/58. Length of Last Word.js
@@ -0,0 +1,14 @@
+/**
+ * @author: angelasubi
+ * @date: 2018-04-28
+ */
+
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function(s) {
+  let arr = s.trim().split(' ')
+  return arr[arr.length - 1].length
+};
