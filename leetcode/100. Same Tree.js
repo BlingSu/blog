8565562f17f824dde61fb47a9f93d54d0813b5b7
angelasubi <21978301@qq.com>
2019-04-30 11:49:43 +0800

    feat: same tree

---
+++ leetcode/100. Same Tree.js
@@ -0,0 +1,23 @@
+/**
+ * @author: angelasubi
+ * @dateL 2019-04-29
+ */
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {boolean}
+ */
+var isSameTree = function(p, q) {
+  if (!p && !q) return true
+  if ((p && !q) || (!p &&q) || (p.val != q.val)) return false
+  
+  return isSameTree(p.left, q.left) && isSameTree(p.right, q.right)
+}
