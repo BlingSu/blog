216e90b1dbc35be5808cbe069ef140991f11c417
angelasubi <21978301@qq.com>
2019-04-30 11:50:06 +0800

    feat: symmetric tree

---
+++ leetcode/101. Symmetric Tree.js
@@ -0,0 +1,26 @@
+/**
+ * @author: angelasubi
+ * @dateL 2019-04-29
+ */
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isSymmetric = function(root) {
+  if (!root) return true
+  return isSame(root.left, root.right)
+}
+
+var isSame = function (left, right) {
+  if (!left && !right) return true
+  if (!left || !right || left.val != right.val) return false
+  return isSame(left.left, right.right) && isSame(left.right, right.left)
+}
